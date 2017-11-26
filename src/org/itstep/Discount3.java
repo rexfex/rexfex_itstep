4386e4f75a1a2d9f36a25675859a95320f4caea0
rexfex777 <rexfex777@ukr.net>
2017-11-26 23:58:48 +0200

    Discount

---
+++ src/org/itstep/Discount3.java
@@ -0,0 +1,72 @@
+package org.itstep;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Discount3 {
+
+	public static void main(String[] args) {
+
+		ArrayList<Goods> List = new ArrayList<Goods>();
+
+		Goods g1 = new Goods(222, 200);
+		Goods g2 = new Goods(222, 200);
+		Goods g3 = new Goods(444, 400);
+		Goods g4 = new Goods(333, 300);
+		Goods g5 = new Goods(444, 400);
+		Goods g6 = new Goods(111, 100);
+		Goods g7 = new Goods(444, 400);
+		Goods g8 = new Goods(222, 200);
+		Goods g9 = new Goods(444, 400);
+		Goods g10 = new Goods(333, 300);
+
+		List.addAll(Arrays.asList(g1, g2, g3, g4, g5, g6, g7, g8, g9, g10));
+		int[] discountArray;
+		discountArray = new int[List.size()];
+		int n = 0, k = 0, l = 0, j = 0;
+		int totalsumm = 0;
+
+		for (int i = 0; i < List.size(); i++) {
+
+			switch (List.get(i).getCode()) {
+			case 111:
+				n = n + 1;
+				if (n % 3 == 0) {
+					discountArray[i] = List.get(i).getPrice() / 2;
+				} else {
+					discountArray[i] = List.get(i).getPrice();
+				}
+				break;
+				
+			case 222:
+				j = j + 1;
+				if (j % 3 == 0) {
+					discountArray[i] = List.get(i).getPrice() / 2;
+				} else {
+					discountArray[i] = List.get(i).getPrice();
+				}
+				break;
+				
+			case 333:
+				k = k + 1;
+				if (k % 3 == 0) {
+					discountArray[i] = List.get(i).getPrice() / 2;
+				} else {
+					discountArray[i] = List.get(i).getPrice();
+				}
+				break;
+				
+			case 444:
+				l = l + 1;
+				if (l % 3 == 0) {
+					discountArray[i] = List.get(i).getPrice() / 2;
+				} else {
+					discountArray[i] = List.get(i).getPrice();
+				}
+				break;
+			}
+			totalsumm = totalsumm + discountArray[i];
+		}
+		System.out.println(totalsumm);
+	}
+}
---
+++ src/org/itstep/Goods.java
@@ -0,0 +1,27 @@
+package org.itstep;
+
+public class Goods {
+	private int code;
+	private int price;
+
+	public Goods(int code, int price) {
+		this.code = code;
+		this.price = price;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
